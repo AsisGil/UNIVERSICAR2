20e9639e88d719d01507fb808624ba8042487a96
Asis Monteiro de Oliveira Gil <asis_torre@hotmail.com>
2017-01-11 18:01:14 +0100

    PRIMER COMMIT

---
+++ app/src/main/java/com/example/usuario/universicar/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.usuario.universicar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        listeners();
+
+    }
+
+    public void listeners() {
+
+    }
+
+
+}
